3797b89b8864c63f8e21a3139c18062be2f29de1
Ali Emad Ali <ali.20352473@eng.aun.edu.eg>
2023-01-21 20:47:01 +0200

    Update Boot_loader_main.c

--- Src/Boot_loader_main.c
+++ Src/Boot_loader_main.c
@@ -36,9 +36,19 @@ int main(void)
 		(void)Boot_Loader_u32EnterProgrammingMode();
 	}
 
+	/*
+	 * Give SCB the new address of the vector table.
+	 * Why? here's an example:
+	 * User uses the vector of address 0x5 + Flash start address defined in
+	 * his linker script. As this definition is not the default one, vector
+	 * table hence is not in its default place.
+	 * Hence, it is shifted it here to avoid user mistake and reduce overhead
+	 * of every time programming.
+	 */
 	MSCB_voidWriteVectorTableOffset(
 		0x08000000 + 1024 * BOOT_LOADER_SIZE_IN_KB, MSCB_VTOR_Code);
 
+	/*	start user's code	*/
 	vvFunc_t start = *(vvFunc_t*)(0x08000004 + 1024 * BOOT_LOADER_SIZE_IN_KB);
 
 	start();
